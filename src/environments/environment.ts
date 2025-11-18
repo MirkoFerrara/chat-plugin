59b81a8e413702cffabd478b4978f7be69c34b80
MirkoFerrara <mirkoferrara.pvt@gmail.com>
2025-11-18 02:09:20 +0100

    rimosso /api dall'url del localhost:8080 nell'environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',  // ⚠️ MODIFICA CON IL TUO
-  wsUrl: 'ws://localhost:8080/ws'        // ⚠️ MODIFICA CON IL TUO
+  apiUrl: 'http://localhost:8080',
+  wsUrl: 'ws://localhost:8080/ws'
 };
